15ccb9c82d22f0ea43e5e0cebc4d2711aad0cde3
MuhamedDzevlan91 <37083038+MuhamedDzevlan91@users.noreply.github.com>
2021-03-12 20:10:32 +0100

    1

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    var myFirstString = "I thinktherefore I \\ am \\ -- sentient";
+    var mySecondString = "No, yo only \\ think \\ , so you arent";
+
+  return{
+    myFirstString,
+    mySecondString
+  };
+}
+
+console.log(main());
+
+module.exports = main;
