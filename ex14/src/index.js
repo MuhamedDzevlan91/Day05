a53b327e765f7534035490771c04e1b4072101a8
MuhamedDzevlan91 <37083038+MuhamedDzevlan91@users.noreply.github.com>
2021-03-12 20:13:31 +0100

    14

---
+++ ex14/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    var myNoung = "dog"
+    var myVerb = "ran"
+    var myAdjective = "big"
+    var myAdverb = "quickly"
+    var wordBlanks = "My " + myNoung + " yesterday " + myVerb + " after one rally " + myAdjective + " dog, and I told him to come back " + myAdverb +"."
+
+    return {
+        wordBlanks
+    };
+ }
+ 
+ console.log(main());
+
+ module+exports=main;
+ 
